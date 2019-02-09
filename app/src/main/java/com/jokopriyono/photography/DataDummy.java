3a0bda08a563d5c006ba7bfb4faac87faf2b9da7
jokopriyono <joe.mandrake007@gmail.com>
2019-02-09 12:07:00 +0700

    Initial commit

---
+++ app/src/main/java/com/jokopriyono/photography/DataDummy.java
@@ -0,0 +1,25 @@
+package com.jokopriyono.photography;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataDummy {
+    private List<String> listDataDummy = new ArrayList<>();
+
+    public DataDummy() {
+        listDataDummy.add("Dummy satu");
+        listDataDummy.add("Dummy dua");
+        listDataDummy.add("Dummy tiga");
+        listDataDummy.add("Dummy empat");
+        listDataDummy.add("Dummy lima");
+        listDataDummy.add("Dummy enam");
+        listDataDummy.add("Dummy tujuh");
+        listDataDummy.add("Dummy delapan");
+        listDataDummy.add("Dummy sembilan");
+        listDataDummy.add("Dummy sepuluh");
+    }
+
+    public List<String> getListDataDummy() {
+        return listDataDummy;
+    }
+}
---
+++ app/src/main/java/com/jokopriyono/photography/MainActivity.java
@@ -0,0 +1,19 @@
+package com.jokopriyono.photography;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recycler = findViewById(R.id.recycler);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+}
